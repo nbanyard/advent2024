388e6baa7be52edb5845b403424df6873c213f49
nbanyard <nbanyard@users.noreply.github.com>
2024-12-15 18:23:30 +0000

    Fixes

--- src/bin/two.rs
+++ src/bin/two.rs
@@ -2,8 +2,8 @@ use std::collections::HashMap;
 use advent2024::data;
 
 fn main() {
-    let m1 = countItems(data.LIST1);
-    let m2 = countItems(data.LIST2);
+    let m1 = countItems(data::LIST1);
+    let m2 = countItems(data::LIST2);
 
     let mut distance: i32 = 0;
     for (item, count1) in m1.iter() {
@@ -14,7 +14,7 @@ fn main() {
 }
 
 fn countItems(items: &[u32]) -> HashMap<u32, u32> {
-    let mut map = HashMap.new();
+    let mut map = HashMap::new();
     for item in items.iter() {
         map.insert(item, map.get(item).unwrap_or(0) + 1);
     }
