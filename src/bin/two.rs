53bc5b3014959b5814b6435e3b1557cd463e79f1
nbanyard <nbanyard@users.noreply.github.com>
2024-12-15 18:18:29 +0000

    Try second challenge

---
+++ src/bin/two.rs
@@ -0,0 +1,22 @@
+use std::collection::HashMap;
+use crate::data;
+
+fn main() {
+    let m1 = countItems(data.LIST1);
+    let m2 = countItems(data.LIST2);
+
+    let mut distance: i32 = 0;
+    for (item, count1) in m1.iter() {
+        count2 = m2.unwrap_or(0);
+        distance += item * count1 * count2;
+    }
+    println!("Distance: {}", distance);
+}
+
+fn countItems(items: &[u32]) -> HashMap<u32, u32> {
+    let mut map = HashMap.new();
+    for item in items.iter() {
+        map.insert(item, map.get(item).unwrap_or(0) + 1);
+    }
+    map
+}
