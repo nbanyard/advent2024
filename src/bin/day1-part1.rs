c7cb9a943726ba41d8b99a82c06e31cf2ca5f22d
nbanyard <nbanyard@users.noreply.github.com>
2024-12-15 18:38:47 +0000

    Day one, part two (#2)

---
+++ src/bin/day1-part1.rs
@@ -0,0 +1,15 @@
+use std::iter::zip;
+use advent2024::data;
+
+fn main() {
+    let mut s1 = Vec::from(data::LIST1);
+    let mut s2 = Vec::from(data::LIST2);
+    s1.sort();
+    s2.sort();
+
+    let mut distance: i32 = 0;
+    for (i1, i2) in zip(s1, s2) {
+        distance += (i1 - i2).abs();
+    }
+    println!("Distance: {}", distance);
+}
---
+++ src/bin/day1-part2.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+use advent2024::data;
+
+fn main() {
+    let m1 = count_items(&data::LIST1);
+    let m2 = count_items(&data::LIST2);
+
+    let mut distance: i32 = 0;
+    for (item, count1) in m1.iter() {
+        let count2 = m2.get(item).unwrap_or(&0);
+        distance += item * count1 * count2;
+    }
+    println!("Distance: {}", distance);
+}
+
+fn count_items(items: &[i32]) -> HashMap<i32, i32> {
+    let mut map = HashMap::new();
+    for item in items.iter() {
+        map.insert(*item, map.get(item).unwrap_or(&0) + 1);
+    }
+    map
+}
