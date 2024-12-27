53ffc524fb2a56b0c27d11eb36aa1bbb75b60c18
nbanyard <nbanyard@yahoo.co.uk>
2024-12-27 13:50:54 +0000

    Day 5, part 2

--- src/bin/day5-part2.rs
+++ src/bin/day5-part2.rs
@@ -4,13 +4,14 @@ use std::collections::HashSet;
 fn main() {
     let data: Vec<&str> = include_str!("day5-data.txt").lines().collect();
     let (rules, updates) = extract_data(&data);
-    let mut valid = 0;
+    let mut fixed = 0;
     for update in updates {
-        if is_valid_update(&update, &rules) {
-            valid += middle_number(&update);
+        if !is_valid_update(&update, &rules) {
+            let fixed_update = fix_update(&update, &rules);
+            fixed += middle_number(&fixed_update);
         }
     }
-    println!("Number of valid updates: {}", valid);
+    println!("Sum of middle of fixed updates: {}", fixed);
 }
 
 fn extract_data(data: &[&str]) -> (HashMap<i32, HashSet<i32>>, Vec<Vec<i32>>) {
@@ -49,6 +50,36 @@ fn is_valid_update(update: &[i32], rules: &HashMap<i32, HashSet<i32>>) -> bool {
         }
     }
     true
+}
+
+fn fix_update(update: &[i32], rules: &HashMap<i32, HashSet<i32>>) -> Vec<i32> {
+    let mut working = Vec::from(update);
+    let mut result = Vec::new();
+
+    'find_next: while !working.is_empty() {
+        for (i, page) in working.iter().enumerate() {
+            match rules.get(page) {
+                None => {
+                    result.push(working.remove(i));
+                    continue 'find_next;
+                }
+                Some(need_first) => {
+                    let mut ok = true;
+                    for later in &working[i + 1..] {
+                        if need_first.contains(later) {
+                            ok = false;
+                            break;
+                        }
+                    }
+                    if ok {
+                        result.push(working.remove(i));
+                        continue 'find_next;
+                    }
+                }
+            }
+        }
+    }
+    result
 }
 
 fn middle_number(update: &[i32]) -> i32 {
